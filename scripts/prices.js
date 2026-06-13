5ee5ed9bfb4b94f5f18be57c6b75835ac0617d50
Kevin Curry <kevin@intangible.ai>
2026-06-12 20:58:18 -0400

    Rename grains price category

--- scripts/prices.js
+++ scripts/prices.js
@@ -31,7 +31,7 @@ const PRICE_FILTER_ICON_MAP = {
   honey: '🐝',
   oysters: '🦪',
   rice: '🌾',
-  'grains & pantry': '🫙',
+  'grains & mill goods': '🌾',
   mushrooms: '🍄',
   microgreens: '🌱',
   'farm boxes': '📦'
